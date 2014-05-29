cea55c694e8f163b98f3ae4fa9999bd2f061544d
saeedSarpas <s.sarpas@gmail.com>
2014-05-30 00:05:35 +0430

    esmfamilHeader directive

--- src/coffee/directives.coffee
+++ src/coffee/directives.coffee
@@ -2,4 +2,7 @@ angular.module("esmfamil.directive", [])
   .directive('esmfamilSlideBox', -> {
     restrict: 'E'
     templateUrl: '../slide_box_temp.html'
+  }).directive('esmfamilHeader', -> {
+    restrict: 'E'
+    templateUrl: '../header_temp.html'
   })
--- www/js/directives.js
+++ www/js/directives.js
@@ -3,6 +3,11 @@ angular.module("esmfamil.directive", []).directive('esmfamilSlideBox', function(
     restrict: 'E',
     templateUrl: '../slide_box_temp.html'
   };
+}).directive('esmfamilHeader', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '../header_temp.html'
+  };
 });
 
 //# sourceMappingURL=directives.js.map
